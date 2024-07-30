5f778283d40843bc08b7226db12a4f8a73828d4c
Sandip shah <49273206+sandippshah@users.noreply.github.com>
2024-07-30 13:20:00 +0530

    Add files via upload

--- plugins/Extra/repo.py
+++ plugins/Extra/repo.py
@@ -35,7 +35,7 @@ async def git(Kashmira, message):
 
 <b>Open Issues :</b> <i>{qw.get("open_issues")}</i>
 
-<b>Powed by :</b> @creatorrio
+<b>Powed by :</b> @spshah878
 
 """
         if qw.get("description"):

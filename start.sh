f465365f7ee76b4bb39f3fd43498f2b3f5cc934e
Sandip shah <49273206+sandippshah@users.noreply.github.com>
2024-09-18 14:00:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/sandippshah/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+  git clone https://github.com/sandippshah/Rx-AutoFiler2.git /Rx-AutoFiler2 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  git clone $UPSTREAM_REPO /Rx-AutoFiler2 
 fi
-cd /VJ-FILTER-BOT 
+cd /Rx-AutoFiler2 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py

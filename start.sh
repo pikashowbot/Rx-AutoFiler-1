08224bcc5090ac1454bd9a8080d640b498c45532
Sandip shah <49273206+sandippshah@users.noreply.github.com>
2024-09-18 15:54:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/sandippshah/Rx-AutoFiler2.git /Rx-AutoFiler2 
+  git clone https://github.com/sandippshah/Rx-AutoFiler2.git /RX-AUTOFILER2 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Rx-AutoFiler2 
+  git clone $UPSTREAM_REPO /RX-AUTOFILER2 
 fi
-cd /Rx-AutoFiler2 
+cd /RX-AUTOFILER2 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
-gunicorn app:app & python3 bot.py
+python3 bot.py

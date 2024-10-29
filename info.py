d359a75141a0cf2a13c513a20477b925e07155c3
Sandip shah <49273206+sandippshah@users.noreply.github.com>
2024-10-29 14:07:36 +0530

    Add files via upload

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ MV_UPDATE_CHANNEL = -1002154519885  # ID of the log of daily movies update CHANN
 SEND_MV_LOGS = bool(environ.get('SEND_MV_LOGS', False)) #send newmovies log to update channel 
 
 auth_channel = environ.get('AUTH_CHANNEL', '-1001641011626')  # public channel 
-second_auth_channel = environ.get('SECOND_AUTH_CHANNEL', '-1002411099541')  # Add the second auth channel or Group (should private)
+second_auth_channel = environ.get('SECOND_AUTH_CHANNEL', '-1002412393169')  # Add the second auth channel or Group (should private)
 third_auth_channel = environ.get('THIRD_AUTH_CHANNEL', '-1002496787620')  # Add the third auth channel or Group (should private)
 
 

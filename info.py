d7247848eca54803612664ad646416c6fcab7908
Sandip shah <49273206+sandippshah@users.noreply.github.com>
2024-11-03 15:21:10 +0530

    Add files via upload

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ SEND_MV_LOGS = bool(environ.get('SEND_MV_LOGS', False)) #send newmovies log to u
 
 auth_channel = environ.get('AUTH_CHANNEL', '-1001641011626')  # public channel 
 second_auth_channel = environ.get('SECOND_AUTH_CHANNEL', '-1002412393169')  # Add the second auth channel or Group (should private)
-third_auth_channel = environ.get('THIRD_AUTH_CHANNEL', '-1002496787620')  # Add the third auth channel or Group (should private)
+third_auth_channel = environ.get('THIRD_AUTH_CHANNEL', '-1002364426082')  # Add the third auth channel or Group (should private)
 
 
 

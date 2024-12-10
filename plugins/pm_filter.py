34c45581be92ac01ba5cd5dc0f1f105a5bf58f65
pikashowbot <pikashowmoviesofficial@gmail.com>
2024-12-10 14:57:45 +0530

    Add files via upload

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -3044,9 +3044,9 @@ async def auto_filter(client, msg, spoll=False, spell_chok=True, **kwargs):
                 cap += f"<b>\n📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n</a></b>"
     else:
         if settings["button"]:
-            cap = f"<b>Rᴇsᴜʟᴛs Sʜᴏᴡ Iɴ:- {remaining_seconds} sᴇᴄᴏɴᴅs\nRᴇsᴜʟᴛs Fᴏʀ:- {message.text}\nRᴇǫᴜᴇsᴛᴇᴅ Bʏ:- {message.from_user.mention}\nGʀᴏᴜᴘ:- {message.chat.title}</b>\nTʜɪs ᴍᴇssᴀɢᴇ ᴡɪʟʟ ʙᴇ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴅᴇʟᴇᴛᴇᴅ ɪɴ 2 ᴍɪɴᴜᴛᴇs..!" 
+            cap = f"<b>Rᴇsᴜʟᴛs Sʜᴏᴡ Iɴ:- {remaining_seconds} sᴇᴄᴏɴᴅs\nRᴇsᴜʟᴛs Fᴏʀ:- {message.text}\nRᴇǫᴜᴇsᴛᴇᴅ Bʏ:- {message.from_user.mention}\nGʀᴏᴜᴘ:- {message.chat.title}</b>\nTʜɪs ᴍᴇssᴀɢᴇ ᴡɪʟʟ ʙᴇ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴅᴇʟᴇᴛᴇᴅ ɪɴ 4 ᴍɪɴᴜᴛᴇs..!" 
         else: 
-            cap = f"<b>Rᴇsᴜʟᴛs Sʜᴏᴡ Iɴ:- {remaining_seconds} sᴇᴄᴏɴᴅs\nRᴇsᴜʟᴛs Fᴏʀ:- {message.text}\nRᴇǫᴜᴇsᴛᴇᴅ Bʏ:- {message.from_user.mention}\nGʀᴏᴜᴘ:- {message.chat.title}</b>\nTʜɪs ᴍᴇssᴀɢᴇ ᴡɪʟʟ ʙᴇ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴅᴇʟᴇᴛᴇᴅ ɪɴ 2 ᴍɪɴᴜᴛᴇs..!"
+            cap = f"<b>Rᴇsᴜʟᴛs Sʜᴏᴡ Iɴ:- {remaining_seconds} sᴇᴄᴏɴᴅs\nRᴇsᴜʟᴛs Fᴏʀ:- {message.text}\nRᴇǫᴜᴇsᴛᴇᴅ Bʏ:- {message.from_user.mention}\nGʀᴏᴜᴘ:- {message.chat.title}</b>\nTʜɪs ᴍᴇssᴀɢᴇ ᴡɪʟʟ ʙᴇ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴅᴇʟᴇᴛᴇᴅ ɪɴ 4 ᴍɪɴᴜᴛᴇs..!"
             cap+="\n<b><u>🍿 Your Movie Files 👇</u></b>\n\n"
             for file in files:
                 cap += f"<b>📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"

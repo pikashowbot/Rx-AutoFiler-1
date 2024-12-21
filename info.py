1db3418a1dbd399194ca82d367352fadefcd16c4
pikashowbot <pikashowmoviesofficial@gmail.com>
2024-12-21 16:18:20 +0530

    Add files via upload

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ SEND_MV_LOGS = bool(environ.get('SEND_MV_LOGS', True)) #send newmovies log to up
 
 
 auth_channel = environ.get('AUTH_CHANNEL', '-1001641011626')  # public channel 
-second_auth_channel = environ.get('SECOND_AUTH_CHANNEL', '-1002446439323')  # Add the second auth channel or Group (should private)
+second_auth_channel = environ.get('SECOND_AUTH_CHANNEL', '-1002300590629')  # Add the second auth channel or Group (should private)
 third_auth_channel = environ.get('THIRD_AUTH_CHANNEL', '-1002408192874')  # Add the third auth channel or Group (should private)
 
 
